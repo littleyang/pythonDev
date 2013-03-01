71adf8cbe69922609d949c338089a593594b6bce
littleyang <littleyangqh@gmail.com>
2013-03-01 15:25:05 +0800

    pyhton dev

---
+++ list.py
@@ -0,0 +1,13 @@
+numbers = []
+strings = []
+names = ["john","Eric","Jess"]
+second_name = names[1]
+numbers.append(1)
+numbers.append(2)
+strings.append("hello")
+strings.append("name")
+print numbers
+print strings
+print second_name
+
+print ("the second name on names is %s" % second_name)
---
+++ variable.py
@@ -0,0 +1,29 @@
+#python is an object oriented language,it is no variable type ,so if you define a variable
+# you can use it directorily
+
+myint = 7
+# this will auto assign 7 to the variable myint and declare the myint as an int type
+# as well as follow will be float
+
+myfloat = 7.0
+
+myString = "this is my string"
+
+# as well ,you can declare two variable and assign them values useing ","
+# as follows
+
+my_int_one,my_int_two = 1,2
+
+# the result
+
+print myint
+
+print myfloat
+
+print myString
+
+print my_int_two
+
+print my_int_one
+
+
